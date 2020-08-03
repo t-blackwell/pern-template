749e5cadb7c2ec981dea6158e5cb7e04f540f4e2
Tom Blackwell <tlb.blackwell@googlemail.com>
2020-08-03 21:21:06 +0100

    clear auth state on logout

--- client/src/redux/reducers/authenticationReducer.tsx
+++ client/src/redux/reducers/authenticationReducer.tsx
@@ -42,7 +42,13 @@ export default function (state = initialState, action: any) {
       localStorage.removeItem('refreshToken');
       localStorage.removeItem('userData');
 
-      return { isAuthenticated: false };
+      return {
+        ...state,
+        isAuthenticated: false,
+        accessToken: null,
+        refreshToken: null,
+        userData: null,
+      };
     default:
       return state;
   }
